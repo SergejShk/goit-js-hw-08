b35cd647aa9ff05c6f5d812d874300c759f9fb5a
SergejShk <sshkurupij11@gmail.com>
2022-06-23 20:30:31 +0300

    add 3 tasks

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
 
+const formRef = document.querySelector('.feedback-form');
+
+const FEEDBACK_KEY = 'feedback-form-state';
+let formData = {};
+
+const populateForm = () => {
+  const savedMessage = localStorage.getItem(FEEDBACK_KEY);
+  formData = JSON.parse(savedMessage) ?? {};
+  const formDataKeys = Object.keys(formData);
+
+  if (savedMessage) {
+    formDataKeys.map(key => {
+      formRef.elements[key].value = formData[key];
+    });
+  }
+};
+
+const onFormInput = evt => {
+  formData[evt.target.name] = evt.target.value;
+  localStorage.setItem(FEEDBACK_KEY, JSON.stringify(formData));
+};
+
+const onFormSubmit = evt => {
+  evt.preventDefault();
+  localStorage.removeItem(FEEDBACK_KEY);
+  // console.log(new FormData(evt.currentTarget));
+  console.log(formData);
+
+  evt.target.reset();
+};
+
+populateForm();
+formRef.addEventListener('input', throttle(onFormInput, 500));
+formRef.addEventListener('submit', onFormSubmit);
